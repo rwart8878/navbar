31ec4eaa21c2aa40c96a883f6a562614e52c8725
Rupam Tiwary <Rupamtiwary258@gmail.com>
2023-03-29 11:36:34 +0530

    initial commit

--- src/component/HeroSection/index.js
+++ src/component/HeroSection/index.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Video from "../../component/Video/video.mp4"
-import { HeroContainer,HeroBg,VideoBg } from './HeroElements';
-
+import { HeroContainer,HeroBg,VideoBg,HeroContent,HeroH1,HeroP,HeroBtnWrapper,ArrowForward,ArrowRight} from './HeroElements';
+import {Button} from '../ButtonElement';
 const HeroSection = () => {
+    const[hover,setHover] = useState(false)
+    const onHover = () => {
+        setHover(!hover)
+    }
   return (
     <HeroContainer>
         <HeroBg>
             <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
         </HeroBg>
+        <HeroContent>
+            <HeroH1>Welcome To The Site</HeroH1>
+            <HeroP>Coming Soon Stay Tuned</HeroP>
+            <HeroBtnWrapper>
+                <Button to="signup" onMouseEnter={onHover} 
+                onMouseLeave={onHover}>
+                    Get started {hover ? <ArrowForward/> : <ArrowRight/>}
+                </Button>
+            </HeroBtnWrapper>
+        </HeroContent>
     </HeroContainer>
     
   )
